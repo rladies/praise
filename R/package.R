3c11ab7cb0240b05f171dcd06491eeee8634e150
ironholds <ironholds@gmail.com>
2015-07-29 23:24:52 -0400

    add .Rproj, project documentation

--- R/package.R
+++ R/package.R
@@ -1,4 +1,15 @@
 
+#' @title Praise Users
+#' @name praise
+#' @description Build friendly R packages that
+#' praise their users if they have done something
+#' good, or they just need it to feel better.
+#' 
+#' @docType package
+#' @aliases praise praise-package
+
+NULL
+
 #' Words and phrases to be used as positive adjectives
 #'
 #' @details
