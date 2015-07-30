fa4497d13c99816c7092ebeb6a964a0ed9546cfb
Gabor Csardi <csardi.gabor@gmail.com>
2015-07-30 17:05:25 -0400

    Move data from data/ to R/ #2

--- R/package.R
+++ R/package.R
@@ -9,13 +9,3 @@
 #' @aliases praise praise-package
 
 NULL
-
-#' Words and phrases to be used as positive adjectives
-#'
-#' @details
-#' Most of them are from \url{https://github.com/sindresorhus/superb}.
-#'
-#' @format
-#' A character vector of words and expressions.
-
-"superb"
