589028f8220fc2442b0fdf6f13831b9d6be9b6e1
Mine Cetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-11-17 10:20:37 +0000

    Add "formidable" to adjectives (#90)
    * Add "formidable" to adjectives
    
    * Fix spelling!
    
    * Add comma after formidable
    
    Co-authored-by: Gábor Csárdi <csardi.gabor@gmail.com>

--- R/adjective.R
+++ R/adjective.R
@@ -1,4 +1,3 @@
-
 adjective <- c(
   "ace",
   "adorable",
@@ -51,6 +50,7 @@ adjective <- c(
   "first-class",
   "first-rate",
   "flawless",
+  "formidable",
   "funkadelic",
   "geometric",
   "glorious",
